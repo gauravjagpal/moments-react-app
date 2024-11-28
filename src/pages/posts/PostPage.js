7d32073326ddd095eca63af9d85d47a922076e9e
gaurav jagpal <gauravjagpal@hotmail.com>
2024-11-28 19:44:52 +0000

    add feed functionality

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -6,33 +6,34 @@ import Container from "react-bootstrap/Container";
 
 import appStyles from "../../App.module.css";
 import { useParams } from "react-router";
-import { axiosReq } from "../../api/axiosDefaults"
+import { axiosReq } from "../../api/axiosDefaults";
+import Post from "./Post";
 
 function PostPage() {
-  const {id} = useParams();
-  const [post, setPost] = useState({resutlts: []});
+  const { id } = useParams();
+  const [post, setPost] = useState({ results: [] });
 
-  useEffect (() => {
+  useEffect(() => {
     const handleMount = async () => {
-        try {
-            const[{ data: post }] = await Promise.all([
-                axiosReq.get(`/posts/${id}`)
-            ]);
-            setPost({resutlts: [post]})
-            console.log(post)
-        } catch (err) {
-            console.log(err)
-        }
-    }
-    handleMount()
-  }, [id])
+      try {
+        const [{ data: post }] = await Promise.all([
+          axiosReq.get(`/posts/2`),
+        ]);
+        setPost({ results: [post] })
+        console.log(post)
+      } catch (err) {
+        console.log(err)
+      }
+    };
+    handleMount();
+  }, [id]);
 
 
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles for mobile</p>
-        <p>Post component</p>
+        <Post {...post.results[0]} setPosts={setPost} postPage />
         <Container className={appStyles.Content}>
           Comments
         </Container>
